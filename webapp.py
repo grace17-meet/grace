7de83fdd509e9b594cdcbdcded07e2ed950bbbfc
yali18-meet <yaellulav@gmail.com>
2017-02-13 19:08:50 +0200

    hi

--- webapp.py
+++ webapp.py
@@ -28,16 +28,74 @@ app.secret_key = "MY_SUPER_SECRET_KEY"
 @app.route('/')
 def showMainPage():
 	return render_template('homepage.html')
+@app.route('/newTaster', methods = ['GET', 'POST'])
+def newTaster():
+	if request.method == 'POST':
+		name= request.form['name']
+		email = request.form['email']
+		password = request.form['password']
+		address = request.form['address']
+		if name =="" or email == "" or password == "":
+			flash ("Your missing some arguments")
+			return redirect(url_for('newTaster'))
+		if session.query(Customer).filter_by(email = email).first() is not None:
+			flash("A user with this email already exists")
+			return redirect(url_for('newTaster'))
+		taster = Taster(name = name, email = email, address = address )
+		taster.hash_password(password)
+		session.add(taster)
+		session.commit()
+		flash("Welcome!")
+	else:
+		return render_template('newTaster.html')
+		flash("Please try again")
 
-@app.route('/')
+@app.route('/login', methods = ['GET','POST'])
+def login():
+    if request.method == 'GET':
+        return render_template("login.html")   
+    elif request.method == 'POST':
+        email = request.form["email"]
+        password = request.form["password"]
+        if email is None or password is None:
+            flash("Missing some Arguments")
+            return redirect(url_for("login"))
+        if verify_password(email,password):
+            customer = session.query(Member).filter_by(email = email).one()
+            flash("Login Succesful,start tasting! %s" % member.name)
+            login_session['name'] = member.name
+            login_session['email'] = member.email
+            login_session['id'] = member.id
+            return redirect(url_for("inventory"))
+        else:
+            flash('Incorrect username/password ')
+            return redirect(url_for("login"))
+            
+@app.route('/logout', methods = ['POST'])
+def logout():
+	return "To be implmented"
+
+
+
 @app.route("/recipes")
 def recipes():
 	recipe = session.query(Recipe).all()
 	return render_template('recipes.html', recipe = recipe)
-@app.route('/')
-def order():
-	
 
+@app.route("/history")
+
+def history():
+	history = session.query(History).all()
+	return render_template('history.html', history = history)
+
+@app.route("/aboutme")
+def aboutme():
+	aboutme = session.query(Aboutme).all()
+	return render_template('aboutme.html', aboutme= aboutme)
+@app.route("/addrecipe")
+def addrecipe():
+	addrecipe = session.query(Addrecipe).all()
+	return render_template('addrecipe.html', addrecipe = addrecipe)
 
 if __name__ == '__main__':
-	app.run(debug=True)
+   app.run(debug=True)
